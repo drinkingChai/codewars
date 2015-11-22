085e80ccab89a087f629b4e4b185e93d455ea091
drinkingTea <teatocode@gmail.com>
2015-11-22 09:27:44 -0500

    added comment

--- number of people in the bus/solution.js
+++ number of people in the bus/solution.js
@@ -1,7 +1,9 @@
 var number = function(busStops){
-  // Good Luck!
   var num_people = 0;
   for (var i = 0; i < busStops.length; i++) {
+  	/*add the number of people getting on and 
+  	remove the number of people getting off
+  	at each stop*/
     num_people += busStops[i][0] - busStops[i][1];
   }
   return num_people;
