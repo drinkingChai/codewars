5a70373ff8de1e6e10cdb39fd2237d102d3039b0
drinkingTea <teatocode@gmail.com>
2015-12-31 03:56:18 -0500

    not fast enough?

---
+++ sum of pairs/solution.js
@@ -0,0 +1,33 @@
+var sum_pairs=function(ints, s){
+    //your code here
+    var return_array = undefined;
+    for (var i = 0; i < ints.length; i++) {
+    	j = s - ints[i];	//j is the number to seek to complete s
+    	sliced = ints.slice(i + 1, ints.length);	//seek through the rest of the array
+    	if (sliced.indexOf(j) != -1) {
+    		//if j is found and the return_array exists,
+    		//check it's second index to see if it is less
+    		//if so, update the array
+    		//otherwise if the array is undefined, update it
+    		if (return_array && sliced.indexOf(j) + i + 1 < return_array[1]) {
+    			return_array = [i, sliced.indexOf(j) + i + 1];
+    		} else {
+    			return_array = return_array || [i, sliced.indexOf(j) + i + 1];
+    		}
+    	}
+    }
+
+    //if the array is undefined, return undefined, otherwise, return the items in s with index of return_array
+    return return_array == undefined ? return_array : return_array.map(function(n) { return ints[n]});
+}
+
+l1= [1, 4, 8, 7, 3, 15];
+l2= [1, -2, 3, 0, -6, 1];
+l3= [20, -13, 40];
+l4= [1, 2, 3, 4, 1, 0];
+l5= [10, 5, 2, 3, 7, 5];
+l6= [4, -2, 3, 3, 4];
+l7= [0, 2, 0];
+l8= [5, 9, 13, -3];
+
+console.log(sum_pairs(l5, 10))
